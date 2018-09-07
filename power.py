58c6c65d391eee5eba7a97555a4cb15531f20dc1
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-09-07 10:05:37 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,13 @@
+def power(a,b):
+        if a==0: return 0
+        elif b==0: return 1
+        elif b==1: return a
+        elif b%2 == 0:
+                res_even = power(a,b/2)
+                return res_even*res_even
+        else :
+                b=(b-1)/2
+                res_odd= power(a,b)
+                return a*res_odd*res_odd
+pow = power(2,10)
+print(pow)
